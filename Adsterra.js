ccb7813f12e363a1a395dd0276b67eff578bc67c
Emon8405 <emon.dhpl@gmail.com>
2026-08-07 16:50:59 +0600

    Delete Adsterra.js

--- Adsterra.js
+++
@@ -1,9 +0,0 @@
-const ADSTERRA_LINK = "https://www.effectivecpmnetwork.com/d7u2jxe2g5?key=9eaaf916e855ccd7f1e9293959c110b0";
-
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".adsterra-link").forEach(function (btn) {
-    btn.href = ADSTERRA_LINK;
-    btn.target = "_blank";
-    btn.rel = "nofollow sponsored";
-  });
-});
